66ec84eb08c540963c4b75a819cfb07949a80982
ErnestoCallejas <89071404+ErnestoCallejas@users.noreply.github.com>
2021-09-24 01:06:58 -0500

    Add files via upload

--- prueba proyecto.py
+++ prueba proyecto.py
@@ -32,20 +32,47 @@ def porcentaje(pregunta5,num1,num2):
 
 pregunta1=str(input("¿las hojas tienen hoyos?  "))
 
+while pregunta1!="si" and pregunta1!="no":
+    print("Respuesta no válida, intente de nuevo")
+    pregunta1=str(input("¿las hojas tienen hoyos?  "))
+
 if pregunta1=="si":
     pregunta2=str(input("¿la planta está caida?  "))
+    
+    while pregunta2!="si" and pregunta2!="no":
+        print("Respuesta no válida, intente de nuevo")
+        pregunta2=str(input("¿la planta está caida?   "))
+    
 elif pregunta1=="no":
     pregunta2=str(input("¿la planta está caída?  "))
+    while pregunta2!="si" and pregunta2!="no":
+        print("Respuesta no válida, intente de nuevo")
+        pregunta2=str(input("¿la planta está caida?    "))
 
 if pregunta2=="si":
-    pregunta3=str(input("¿¿la planta tiene bolas en la raíz?  "))
+    pregunta3=str(input("¿la planta tiene bolas en la raíz?  "))
+    while pregunta3!="si" and pregunta3!="no":
+        print("Respuesta no válida, intente de nuevo")
+        pregunta3=str(input("¿la planta tiene bolas en la raíz? "))
+
 elif pregunta2=="no":
     pregunta3=str(input("¿¿la planta tiene bolas en la raíz?  "))
+    while pregunta3!="si" and pregunta3!="no":
+        print("Respuesta no válida, intente de nuevo")
+        pregunta3=str(input("¿la planta tiene bolas en la raíz? "))
         
 if pregunta3=="si":
     pregunta4=str(input("¿sus hojas tienen manchas?  "))
+    while pregunta4!="si" or pregunta4!="si":
+        print("Respuesta no válida, intente de nuevo")
+        pregunta4=str(input("¿sus hojas tienen manchas?  "))
+
     if pregunta4=="si":
         pregunta5=float(input("En una escala del 1 al 5, donde 5 son todas las hojas y 0 ninguna ¿Cuantas hojas tienen manchas? "))
+        while pregunta5>5:
+            print("Respuesta no válida, intente de nuevo")
+            pregunta5=float(input("En una escala del 1 al 5, donde 5 son todas las hojas y 0 ninguna ¿Cuantas hojas tienen manchas? "))
+
     elif pregunta4=="no":
         pregunta5=0
     else:
