0bc6b095fb4d3c10cae5d209b1c8b42880f4d13a
ErnestoCallejas <89071404+ErnestoCallejas@users.noreply.github.com>
2021-10-15 21:09:48 -0500

    Add files via upload

--- prueba proyecto.py
+++ prueba proyecto.py
@@ -1,16 +1,8 @@
 #Este es el programa con la correción del error de la entrega pasada, los condicionales siguen igual ya que estos si funcionaban.#
 #Las preguntas se responden mediante si y no, para la pregunta de la escala si se usan valores numéricos#
 
-plaga={"Virus":[3],"Bacteria":[1]}
+plaga=["Bacteria","Virus"]
 puntos_iniciales=0
-
-def plaga_r (lista,suma_puntos):
-    for clave,valor in lista.items():
-        if valor[0]==suma_puntos:
-            print(clave[0])
-        elif suma_puntos<valor[0]:
-            print(clave[0])
-    return clave
         
 def cant_puntos1(respuesta_usuario,respuesta_esperada,puntos_iniciales):
     if pregunta1=="si":
@@ -92,6 +84,9 @@ elif pregunta3=="no":
     
 suma_puntos=cant_puntos1(pregunta1,"si",puntos_iniciales)+cant_puntos2(pregunta2,"si",cant_puntos1(pregunta1,"si",puntos_iniciales))+cant_puntos3(pregunta3,"si",cant_puntos2(pregunta2,"si",(cant_puntos1(pregunta1,"si",puntos_iniciales))))
 
-print("tu planta tiene: ",plaga_r(plaga,suma_puntos),",con un porcentaje de daño en hojas del ",porcentaje(pregunta5,100,5),"%")
+if suma_puntos>=2:
+    print("Tu planta tiene: ",plaga[0])
+else:
+    print("tu planta tiene: ",plaga[1],",con un porcentaje de daño en hojas del ",porcentaje(pregunta5,100,5),"%")
 
    
